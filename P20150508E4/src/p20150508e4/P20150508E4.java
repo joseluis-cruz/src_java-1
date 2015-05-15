62f5dee39e558d80ea501f1f867d1d83b01627be
José Luis Cruz <joseluis.cruz@murciaeduca.es>
2015-05-15 12:29:45 +0200

    Cambio para el ejercicio

--- P20150508E4/src/p20150508e4/P20150508E4.java
+++ P20150508E4/src/p20150508e4/P20150508E4.java
@@ -2,6 +2,9 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ *
+ * Modificación de José Luis Cruz para ejercicio
+ *
  */
 package p20150508e4;
 
